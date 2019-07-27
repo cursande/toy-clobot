92907b93d050e929e3e510861a603720285f13b3
Alex F <adfarries@gmail.com>
2019-07-27 12:30:52 +1000

    Add function for parsing the command-type

--- src/toy_clobot/core.clj
+++ src/toy_clobot/core.clj
@@ -1,4 +1,5 @@
-(ns toy-clobot.core)
+(ns toy-clobot.core
+  (:require [clojure.string :refer [split split-lines]]))
 
 (def board-dimensions
   "Default width and height of board in units"
@@ -7,3 +8,25 @@
 (def directions
   "Valid directions"
   [:NORTH :EAST :SOUTH :WEST])
+
+(defn parse-command-type
+  "returns a keyword representing the type of command that will be attempted"
+  [command]
+  (let [command-type (-> command
+                         (split #" ")
+                         (first))
+        command-matches? (fn [command pattern] (boolean (re-find pattern command)))]
+    (cond
+      (command-matches? command-type #"PLACE")
+      :place
+      (command-matches? command-type #"(NORTH|EAST|SOUTH|WEST)")
+      :direction
+      (command-matches? command-type #"MOVE")
+      :move
+      (command-matches? command-type #"REPORT")
+      :report)))
+
+(defn -main [arg]
+  (let [commands (-> arg
+                     (slurp)
+                     (split-lines))]))
--- test/toy_clobot/core_test.clj
+++ test/toy_clobot/core_test.clj
@@ -1,7 +1,17 @@
 (ns toy-clobot.core-test
   (:require [clojure.test :refer :all]
-            [toy-clobot.core :refer :all]))
+            [toy-clobot.core :refer [parse-command-type]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-parse-command-type
+  (testing "it correctly recognises a place command"
+    (let [command "PLACE 1,4,NORTH"]
+      (is (= (parse-command-type command)
+             :place))))
+  (testing "it correctly recognises a direction command"
+    (let [command "EAST"]
+      (is (= (parse-command-type command)
+             :direction))))
+  (testing "it correctly recognises a MOVE-type command"
+    (let [command "MOVE"]
+      (is (= (parse-command-type command)
+             :move)))))
