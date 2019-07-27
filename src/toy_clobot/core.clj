65de868b970a678d5f4f20ecd747899fc9adcf7b
Alex F <adfarries@gmail.com>
2019-07-27 20:39:23 +1000

    Develop working draft solution
    - A lot of this functionality really belongs in another namespace
    - Will need higher-level tests for the core namespace

--- src/toy_clobot/core.clj
+++ src/toy_clobot/core.clj
@@ -1,12 +1,12 @@
 (ns toy-clobot.core
   (:require [clojure.string :refer [split split-lines]]))
 
-;; - Handle the next command [ ]
+;; - Handle the next command [x]
 ;;   - placement [x]
 ;;   - direction change [x]
-;;   - a forward move [ ]
+;;   - a forward move [x]
 ;;     - CHECK if next move is valid, skip if not valid
-;;   - reporting [ ]
+;;   - reporting [x]
 ;;     - print (x: x, y: y, dir: f) to stdout
 ;;   - Dockerise it [  ]
 
@@ -18,9 +18,8 @@
   "Valid directions"
   [:NORTH :EAST :SOUTH :WEST])
 
-(defn parse-command-type
+(defn parse-command-type [command]
   "returns a keyword representing the type of command that will be attempted"
-  [command]
   (let [command-type (-> command
                          (split #" ")
                          (first))
@@ -35,26 +34,95 @@
       (command-matches? command-type #"REPORT")
       :report)))
 
-(defn set-robot-position
-  "Place the robot in valid coordinates"
-  [x y f]
-  (let [valid-x-and-y? (fn [x y]
-                         (and (<= x (:width table-dimensions))
-                              (> x 0)
-                              (<= y (:height table-dimensions))
-                              (> y 0)))]
-    (if (valid-x-and-y? x y)
-      {:x x :y y :f f}
-      {})))
+(defn coords-after-place [command coordinates]
+  "Returns new coords if valid, returns original coordinates if invalid"
+  (let [new-coordinates (-> (re-find #"PLACE (\d+),(\d+),(\w+)" command)
+                            (parse-place-command))]
+    (if (valid-coords? new-coordinates)
+      new-coordinates
+      coordinates)))
+
+(defn valid-coords? [coordinates]
+  "Ensure coords are still on the table"
+  (let [x (:x coordinates)
+        y (:y coordinates)]
+    (and (<= x (:width table-dimensions))
+         (>= x 0)
+         (<= y (:height table-dimensions))
+         (>= y 0))))
+
+(defn parse-place-command [[command x y f]]
+  {:x (read-string x)
+   :y (read-string y)
+   :f (keyword f)})
 
 (defn new-direction [current turn-dir]
- "Will just throw for now if the direction is not valid"
   (let [current-index (.indexOf directions current)] ; TODO: This is pretty gross
-  (case turn-dir
-    :RIGHT (get directions (+ current-index 1) :NORTH)
-    :LEFT (get directions (- current-index 1) :WEST))))
+    (case turn-dir
+      :RIGHT (get directions (+ current-index 1) :NORTH)
+      :LEFT (get directions (- current-index 1) :WEST))))
 
-(defn -main [arg]
-  (let [commands (-> arg
-                     (slurp)
-                     (split-lines))]))
+(defn coords-after-turn [command coordinates]
+  "Returns new coordinates after turning"
+  (assoc coordinates
+         :f
+         (new-direction (:f coordinates) (keyword command))))
+
+(defn coords-after-move [coordinates]
+  "Returns new coordinates if move is valid, returns original coordinates if invalid"
+  (let [x (:x coordinates)
+        y (:y coordinates)
+        new-coordinates (case (:f coordinates)
+                          :NORTH (assoc coordinates :y (inc y))
+                          :EAST  (assoc coordinates :x (inc x))
+                          :SOUTH (assoc coordinates :y (dec y))
+                          :WEST  (assoc coordinates :x (dec x)))]
+    (if (valid-coords? new-coordinates)
+      new-coordinates
+      coordinates)))
+
+(defn report-and-return-coords [coordinates]
+  "returns the coordinates after printing to output stream"
+  (let [report (format "Current Position: [ X: %s, Y: %s, F: %s ]"
+                       (:x coordinates)
+                       (:y coordinates)
+                       (name (:f coordinates)))]
+    (println report)
+    coordinates))
+
+(defn no-command-remaining? [command]
+  (false? (boolean (seq command))))
+
+(defn process-command [command-type command coordinates]
+  "Processes valid actions for the robot, and returns its new coordinates"
+  (if (= command-type :place)
+    (coords-after-place command coordinates)
+    (if (empty? coordinates) ; don't bother processing commands if the robot is not on the table
+      coordinates
+      (case command-type
+        :turn (coords-after-turn command coordinates)
+        :move (coords-after-move coordinates)
+        :report (report-and-return-coords coordinates)))))
+
+;; TODO: Maybe representing the robot/coords with something like an atom would be better..?
+(defn run-robot-commands [commands]
+  (loop [[command & remaining] commands coordinates {}]
+    (if (no-command-remaining? command)
+      (println "All commands complete")
+      (let [command-type (parse-command-type command)
+            new-coordinates (process-command command-type
+                                             command
+                                             coordinates)]
+        (recur remaining new-coordinates)))))
+
+;; (defn -main [arg]
+;;   (let [commands (-> arg
+;;                      (slurp)
+;;                      (split-lines))]
+;;     (run-robot-commands commands)))
+
+(def commands (-> "test/fixtures/instructions.txt"
+                  (slurp)
+                  (split-lines)))
+
+(run-robot-commands commands)
