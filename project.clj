e10ad7ffd2fe07e4a69d1e79b2a117034c2a1667
Alex F <adfarries@gmail.com>
2019-07-29 19:13:49 +1000

    Update readme and project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject toy-clobot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simulates a small lonely robot on a table."
+  :url "https://github.com/cursande/toy-clobot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns toy-clobot.core})
+  :repl-options {:init-ns toy-clobot.core}
+  :main toy-clobot.core)
